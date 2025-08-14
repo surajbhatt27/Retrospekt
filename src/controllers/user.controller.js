787b6e84c7f20aaf3ce37576ab5f010cf5ad1556
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-08-14 20:00:58 +0530

    feat: rename login function to loggedInUser and implement logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -47,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 
-const logIn = asyncHandler(async (req, res) => {
+const loggedInUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body;
     if(!email) {
         throw new ApiError(500, "Email is required");
@@ -85,7 +85,33 @@ const logIn = asyncHandler(async (req, res) => {
             )
 })
 
+const logOutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"));
+});
+
 export { 
     registerUser,
-    logIn
+    loggedInUser,
+    logOutUser
 };

2aaa4f810767a28bbab181c724e7a3fe973a0293
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-08-18 18:52:08 +0530

    fix: correct import path for config and add user routes integration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
-import { config } from "dotenv.js";
+import { config } from "./config/env.js";
 
 const app = express();
 
@@ -13,5 +13,9 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
+
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
 
 export {app};
